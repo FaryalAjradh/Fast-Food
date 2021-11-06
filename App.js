0e1bea3b31be439ba282aa4e997e1df39a4b74e7
Fariyal Ajrad <ajradfariyal@gmail.com>
2021-11-06 14:43:07 +0530

    Add login register screen and create unique favourites for each user

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
+import * as firebase from "firebase";
 
 import {
   useFonts as useOswald,
@@ -10,11 +11,22 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 import { Navigation } from "./src/infrastructure/navigation";
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA-RQcSXFFKcFOGNN5gCXWzlUUoXaJiRk",
+  authDomain: "fast-food7.firebaseapp.com",
+  projectId: "fast-food7",
+  storageBucket: "fast-food7.appspot.com",
+  messagingSenderId: "525639511584",
+  appId: "1:525639511584:web:08cd63e869218a787fd645"
+};
+if(!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
+
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -30,13 +42,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+        <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
